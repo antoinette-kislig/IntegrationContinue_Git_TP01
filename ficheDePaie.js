f1b0b1db87d29edf5eee2b0ca381a1f83b872809
AL Baille <abaille@campus-eni.fr>
2021-02-03 15:35:38 +0100

    Git TP01 V1.1

--- ficheDePaie.js
+++ ficheDePaie.js
@@ -6,9 +6,9 @@
     }
      
     function effacer() {
-      const elemResult = new ElementResultat();
-      const ids = ['salaireBrutFinal','taxeCalculee', 'assuranceCalculee', 'bonusCalcule', 'salaireNet'];
-      for (var i = 0; i < ids.length; i++) {
+      let elemResult = new ElementResultat();
+      let ids = ['salaireBrutFinal','taxeCalculee', 'assuranceCalculee', 'bonusCalcule', 'salaireNet'];
+      for (let i = 0; i < ids.length; i++) {
         elemResult.Tag(ids[i], '');
       }
     }
