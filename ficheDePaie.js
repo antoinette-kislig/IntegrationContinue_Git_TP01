fcdd0521db751acc3cb1de61b1d3c92424d4c131
antoinette-kislig <antoinette_hoang@yahoo.fr>
2021-12-07 15:05:30 +0100

    Ajout de la fonction FicheDePaie

--- ficheDePaie.js
+++ ficheDePaie.js
@@ -24,10 +24,48 @@ function ElementResultat() {
     };
 }
 
+//Représente les données de la fiche de paie
+function FicheDePaie() {
+    const PCT_TAXE = 0.12;
+    const PCT_ASSURANCE = 0.07;
+    const BONUS = 100;
+    const ALLOCATION = 150;
+    const REDUCTION_IMPOT = 0.02;
+    this.salaireBrut;
+    this.isBonus;
+    this.isAllocation;
+    this.allocationCalculee;
+    this.persACharge;
+    this.getSupplement = function () {
+        let supplement = 0;
+        if (this.isBonus === true) {
+            supplement += BONUS;
+        }
+        if (this.isAllocation === true) {
+            supplement += ALLOCATION;
+        }
+        return supplement;
+    };
+
+    this.getAssurance = function () {
+        return this.salaireBrut * PCT_ASSURANCE;
+    };
+    this.getTaxeCalculee = function () {
+        let pctTaxeCalcule = PCT_TAXE;
+        if (this.persACharge >= 3) {
+            pctTaxeCalcule -= REDUCTION_IMPOT;
+        }
+        return this.salaireBrut * pctTaxeCalcule;
+    };
+    this.getSalaireNet = function () {
+        return this.salaireBrut - this.getAssurance() - this.getTaxeCalculee() + this.getSupplement();
+    };
+}
+
 function effacer() {
-  let elemResult = new ElementResultat();
-  let ids = ['salaireBrutFinal','taxeCalculee', 'assuranceCalculee', 'bonusCalcule', 'salaireNet'];
-  for (let i = 0; i < ids.length; i++) {
-    elemResult.Tag(ids[i], '');
-  }
+    let elemResult = new ElementResultat();
+    let ids = ['salaireBrutFinal','taxeCalculee', 'assuranceCalculee', 'bonusCalcule', 'salaireNet'];
+    for (let i = 0; i < ids.length; i++) {
+        elemResult.Tag(ids[i], '');
+    }
 }
