37f0519fe883d2f77bf7f81a79ca8483c23edf07
antoinette-kislig <antoinette_hoang@yahoo.fr>
2021-12-07 14:59:04 +0100

    Ajout de la fonction ElementFormulaire

--- ficheDePaie.js
+++ ficheDePaie.js
@@ -1,14 +1,19 @@
-//TODO Compléter   
-   
-     
-    function calculerSalaireNet() {
-      
-    }
-     
-    function effacer() {
-      let elemResult = new ElementResultat();
-      let ids = ['salaireBrutFinal','taxeCalculee', 'assuranceCalculee', 'bonusCalcule', 'salaireNet'];
-      for (let i = 0; i < ids.length; i++) {
-        elemResult.Tag(ids[i], '');
-      }
-    }
+//Permet de gérer les champs de saisie du formulaire
+function ElementFormulaire() {
+    this.InputCheckbox = function (idInput) {
+        let input = document.getElementById(idInput);
+        return input.checked;
+    };
+    this.InputInt = function (idInput) {
+        let input = document.getElementById(idInput);
+        return parseInt(input.value);
+    };
+}
+
+function effacer() {
+  let elemResult = new ElementResultat();
+  let ids = ['salaireBrutFinal','taxeCalculee', 'assuranceCalculee', 'bonusCalcule', 'salaireNet'];
+  for (let i = 0; i < ids.length; i++) {
+    elemResult.Tag(ids[i], '');
+  }
+}
